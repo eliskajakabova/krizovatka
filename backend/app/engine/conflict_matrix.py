1ede3861ec33ec7fdde5ce30eabddb611a22c90b
Eliška <elehor16@gmail.com>
2026-04-22 11:10:11 +0200

    matrix

--- backend/app/engine/conflict_matrix.py
+++ backend/app/engine/conflict_matrix.py
@@ -1,15 +1,15 @@
 CONFLICT_MATRIX: dict[str, list[str]] = {
-    "N_S": ["E_S", "W_S", "S_L"],
-    "N_L": ["S_S", "S_R", "E_L", "W_R"],
+    "N_S": ["W_L", "W_S", "E_S", "S_L"],
+    "N_L": ["E_S", "S_S", "E_L", "W_L"],
     "N_R": [],
-    "S_S": ["E_S", "W_S", "N_L"],
-    "S_L": ["N_S", "N_R", "W_L", "E_R"],
+    "S_S": ["E_S", "W_S", "N_L", "E_L"],
+    "S_L": ["W_S", "N_S", "W_L", "E_L"],
     "S_R": [],
-    "E_S": ["N_S", "S_S", "W_L"],
-    "E_L": ["W_S", "W_R", "S_L", "N_R"],
+    "E_S": ["N_S", "S_S", "W_L", "N_L"],
+    "E_L": ["S_S", "S_L", "W_S", "N_L"],
     "E_R": [],
-    "W_S": ["N_S", "S_S", "E_L"],
-    "W_L": ["E_S", "E_R", "N_L", "S_R"],
+    "W_S": ["N_S", "S_S", "E_L", "S_L"],
+    "W_L": ["E_S", "N_L", "N_S", "S_S"],
     "W_R": [],
 }
 
